4dcd06182de146c75c941408347d5eb9d05ca43a
Isadeveloppe <151643598+Isadeveloppe@users.noreply.github.com>
2024-09-04 15:10:29 +0200

    Add files via upload

---
+++ templates-parts/burger.php
@@ -0,0 +1,14 @@
+<div class="burger-menu" id="menu-modal">
+    
+    <nav id="nav-menu" class="menu-modal">
+        <?php wp_nav_menu(array(
+            'theme_location' => 'header',
+            'container'      => false,
+            'menu_class'     => 'links',
+        )); ?>
+    </nav>
+</div>
+
+
+
+
